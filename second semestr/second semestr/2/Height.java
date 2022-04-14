502f5c4a57a87fbd600e209854bdf20c76307674
n3tw0rker <62235112+n3tw0rker@users.noreply.github.com>
2022-04-14 23:58:14 +0300

    Delete second semestr/second semestr directory

--- second semestr/second semestr/2/Height.java
+++
@@ -1,3 +0,0 @@
-class Height {
-    int height = 0;
-}
--- second semestr/second semestr/4/hash.java
+++
@@ -1,9 +0,0 @@
-public class hash<K, V> {
-    K key;
-    V value;
-
-    public hash(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-}
